bd4e1ba95fd344100d2d2cea23a53cd204e72231
elprofedotti <elprofedotti@gmail.com>
2023-02-21 21:32:13 -0300

    empezamos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(funcion(1,2,3));
     }
+    public static int funcion(int a, int b, int c){
+        return a+b+c;
+    }
+
 }
