1b2b31b20b68416ba370246892edc0360c361500
elprofedotti <elprofedotti@gmail.com>
2023-02-21 22:24:22 -0300

    actualizado

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import static java.lang.System.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        //añado comentario para probar GIT
         System.out.println(funcion(1,2,3));
 
+        //añado comentario para probar GIT
         Coche coche=new Coche();
-        coche.incrementarPuertas();
         coche.incrementarPuertas();
         System.out.println(coche.puertas);
     }
