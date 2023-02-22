d3c18f73f13f68e26e2427c658d066f9acc83077
elprofedotti <elprofedotti@gmail.com>
2023-02-21 21:50:30 -0300

    empezamos

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(funcion(1,2,3));
+
+        Coche coche=new Coche();
+        coche.incrementarPuertas();
+        coche.incrementarPuertas();
+        System.out.println(coche.puertas);
     }
     public static int funcion(int a, int b, int c){
         return a+b+c;
